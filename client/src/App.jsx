e33daa7e2bfa1b4c3a7d46a3e0329645180d0b3d
OP <ovidiu.ps@gmail.com>
2024-07-22 23:31:03 +0300

    Switch client from react to solid

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import solidLogo from "./assets/solid.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Route } from "@solidjs/router";
+import { Router } from "@solidjs/router";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input></input>
+      <Router>
+        <Route path="/artists" component={test("asdasd")} />
+        <Route path="/artist" component={test("wrrqwe")} />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+function test(t) {
+  return () => <h1>{t}</h1>;
+}
+export default App;
