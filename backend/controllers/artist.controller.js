49223045f37f5c334861ac46bab855cf5c894cf9
OP <ovidiu.ps@gmail.com>
2024-07-22 23:30:44 +0300

    More root changes

--- backend/controllers/artist.controller.js
+++ backend/controllers/artist.controller.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { artistModel } from "../models/artist";
 
 export async function getAll(c, next) {
@@ -29,6 +30,37 @@ export async function getAllAlbums(c, next) {
     return c.json(allAlbums);
   } catch (error) {
     console.log("Could not get albums", error);
+    return c.notFound();
+  }
+}
+
+export async function getArtistAlbums(c, next) {
+  try {
+    const artistId = await c.req.param("id");
+    console.log(artistId);
+    const albumNames = await artistModel.aggregate([
+      {
+        $match: {
+          _id: new ObjectId(artistId),
+        },
+      },
+      {
+        $unwind: {
+          path: "$albums",
+        },
+      },
+      {
+        $project: {
+          title: "$albums.title",
+          songs: "$albums.songs",
+          description: "$albums.description",
+        },
+      },
+    ]);
+
+    return c.json(albumNames);
+  } catch (error) {
+    console.log("Could not get album names", error);
     return c.notFound();
   }
 }
@@ -74,8 +106,10 @@ export async function updateArtist(c) {
   try {
     const _id = await c.req.param("id");
     const update = await c.req.json();
-    const artist = await artistModel.findByIdAndUpdate({_id},update, {new:true})
-    return c.json(artist)
+    const artist = await artistModel.findByIdAndUpdate({ _id }, update, {
+      new: true,
+    });
+    return c.json(artist);
   } catch (error) {
     const errorResponse = {
       error,
@@ -89,8 +123,8 @@ export async function updateArtist(c) {
 export async function deleteArtist(c) {
   try {
     const _id = await c.req.param("id");
-    const artist = await artistModel.deleteOne({_id})
-    return c.json(artist)
+    const artist = await artistModel.deleteOne({ _id });
+    return c.json(artist);
   } catch (error) {
     const errorResponse = {
       error,
--- backend/routes/artist.js
+++ backend/routes/artist.js
@@ -5,19 +5,17 @@ import {
   getAll,
   getAllAlbums,
   getAllArtistsNames,
+  getArtistAlbums ,
   updateArtist
 } from "../controllers/artist.controller";
 const artistRouter = new Hono();
 
-artistRouter.get("/", getAllArtistsNames);
-artistRouter.patch("/:id", updateArtist);
-artistRouter.delete("/:id", deleteArtist);
 artistRouter.get("/all", getAll);
 artistRouter.get("/all-albums", getAllAlbums);
+artistRouter.get("/", getAllArtistsNames);
+artistRouter.get("/:id/albums",getArtistAlbums );
+artistRouter.patch("/:id", updateArtist);
+artistRouter.delete("/:id", deleteArtist);
 artistRouter.post("/", createArtist);
-artistRouter.get("/:id", (c) => {
-  const id = c.req.param("id");
-  return c.json({ name: id });
-});
 
 export default artistRouter;
